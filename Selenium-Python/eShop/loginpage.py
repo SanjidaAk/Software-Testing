4909fef0e950433eeb3b65c0c9458890db675525
SanjidaAk <a.sanjida18@gmail.com>
2024-05-25 13:06:14 +0600

    loginpage.py

---
+++ Selenium-Python/eShop/loginpage.py
@@ -0,0 +1,19 @@
+class LoginPage():
+     login_btn = 'ico-login'
+     email = 'Email'
+     passw = 'Password'
+     remember_me = 'RememberMe'
+     login_klik = 'login-button'
+     successlogin_url ='https://demowebshop.tricentis.com/'
+     error_false_pass = 'validation-summary-errors'
+
+class LoginData():
+        url = 'https://demowebshop.tricentis.com/'
+        url_login = 'https://demowebshop.tricentis.com/login'
+        title = 'Demo Web Shop'
+        email = 'steeleangel03@gmail.com'
+        email_not_found = 'steele@gmail.com'
+        passw = 'steeleangel8'
+        passw_false = 'steeleangel'
+        false_pass_msg = 'Login was unsuccessful. Please correct the errors and try again.'
+        email_notfound_msg = 'No customer account found'
